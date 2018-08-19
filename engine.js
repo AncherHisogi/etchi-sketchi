eafab19ee6b6afe83ce4ca434c77146aa5b739d7
Ancher Tunchar <anchertunchar@gmail.com>
2018-08-20 00:14:31 +0300

    added jquery, added logic for drawing, reseting and setting grid

--- engine.js
+++ engine.js
@@ -2,13 +2,17 @@ let divNew = document.createElement("div");
 let htmlStyles = window.getComputedStyle(document.querySelector("html"));
 const rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
 const colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+const buttonReset_div = document.getElementById("buttonReset");
+const rgbBling_div = document.getElementById("rgbBling");
 let x = 0;
 let y = 0;
-y = window.prompt("gib number 1-64");
+
+
+
 
 function setGrid()
 {
-    
+    y = window.prompt("gib number 1-64");
     document.documentElement.style.setProperty("--rowNum", y);
     document.documentElement.style.setProperty("--colNum", y);
 }
@@ -17,23 +21,47 @@ function input()
 {
     if (y >= 65)
     {
-        window.prompt("NUBAH TOO BIG");
+        alert("NUBAH TOO BIG");
         y = 0;
     }
-
+    
     y = y * y;
     return y;
     
 }
 
-setGrid();
-input();
 
-
-for (x=0; x<y; x++)
+function createDiv()
 {
-    divNew = document.createElement("div");
-    divNew.className = "divKek";
+    for (x=0; x<y; x++)
+    {
+        divNew = document.createElement("div");
+        divNew.classList = "divKek";
 
-    document.getElementById("main").appendChild(divNew);
+        document.getElementById("main").appendChild(divNew);
+    }
+
 }
+
+
+buttonReset_div.addEventListener('click',function()
+{
+    $( "div" ).remove(".divKek");
+    setGrid();
+    input();
+    createDiv();
+    coloring();
+})
+
+
+function coloring() 
+{
+    const squares = document.querySelectorAll("#main");
+    for(let i = 0; i < squares.length; i++)
+    {
+      squares[i].addEventListener("mouseover", function(e){
+        e.target.style.cssText = "background-color: orange";
+      });
+    }
+}
+
